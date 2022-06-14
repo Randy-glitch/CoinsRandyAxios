38a46d81c8f11dc12ede8f02da76240d10b51ad8
Randy <84881845+Randy-glitch@users.noreply.github.com>
2022-06-14 05:55:17 +0200

    Add files via upload

--- coinsapp.js
+++ coinsapp.js
@@ -1,7 +1,7 @@
+
 const form = document.querySelector(".formulario")
 const img = document.querySelector(".img").src
 var  dollar =  54.930
-
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -14,12 +14,35 @@ form.addEventListener("submit", (e) => {
 
         res.data.forEach(e => {
             if ((inp.toLowerCase() === e.symbol || inp.toLowerCase() === e.id)){
+
+                    // jodiendo con fechas y moments JS
+                    var fecha = e.last_updated
+                    var horaGood = fecha.substring(12, 19 )
+                    var fechaGood = fecha.substring(0,4)+''+(fecha.substring(5,7))+''+(fecha.substring(8,10))+horaGood
+                    var hola = moment(fechaGood, "YYYYMMDD, h:mm:ss a").endOf('hour').fromNow(); 
                     document.querySelector(".img").src = e.image
                     document.querySelector(".current").innerHTML = e.current_price
-                    document.querySelector(".change").innerHTML = e.price_change_percentage_24h+" %"
+                    document.querySelector(".change").innerHTML = e.price_change_percentage_24h > 0 ? `${e.price_change_percentage_24h}` : `${e.price_change_percentage_24h}`+" %"
                     document.querySelector(".rd").innerHTML = " RD$ "+parseFloat(e.current_price*dollar) 
+                    document.querySelector("#name").innerHTML = e.name 
+                    document.querySelector(".market_cap_rank").innerHTML = e.market_cap_rank 
+                    document.querySelector(".price_change_24h").innerHTML = e.price_change_24h 
+                    document.querySelector(".ts").innerHTML = e.total_supply 
+                    document.querySelector(".alt").innerHTML = e.atl 
+                    document.querySelector(".ath").innerHTML = e.ath 
+                    document.querySelector(".last_update").innerHTML =  hola
                     
                     
+                }
+                else{
+
+                    const toast = document.querySelector(".toast")
+                    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+                    var toastList = toastElList.map(function (toastEl) {
+                    return new bootstrap.Toast(toastEl, option)
+                    })
+
+
                 }
             });
         })
