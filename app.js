83e540566ad446a30ea14aa9c520d72a4d257f4d
Randy <84881845+Randy-glitch@users.noreply.github.com>
2022-06-21 18:05:33 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -12,15 +12,15 @@ axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=
     }
     document.querySelector(".tableCoins").innerHTML += `
     
-    <tr>
+    <tr class="fila" id="${e.name.toLowerCase()}">
     <th scope="row" class="uid">${e.market_cap_rank}</th>
-    <td colspan="2">
+    <td colspan="2" class="${e.name.toLowerCase()}">
     <img src="${e.image}" style="width:20px;"/>    
     ${e.name} 
-    <span class="ms-3 text-muted text-uppercase">${e.symbol}</span>
+    <span class="${e.symbol} ms-3 text-muted text-uppercase">${e.symbol}</span>
     </td>
     <td>${e.current_price}</td>
-    <td class="${texcolor}">${e.price_change_percentage_24h}</td>
+    <td class="${texcolor}">${e.price_change_24h}</td>
     </tr>
     
     
@@ -66,8 +66,49 @@ axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=
             
         })
 
-        console.log(res.data)
     })
+
+const ty = document.querySelector(".coin")
+const form = document.querySelector(".formulario")
+
+ty.addEventListener("keyup", async (re)=>{
+    re.preventDefault()
+    await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+        .then(res =>  res.data.map(e => {
+            if(re.target.value.toLowerCase() === e.name.toLowerCase() || re.target.value.toLowerCase() === e.symbol.toLowerCase()){
+
+                if (re.target.value != ""){
+                    
+                    $(`.fila`).hide()
+                    $(`#${e.name.toLowerCase()}`).show()
+                    setTimeout(myStopFunction, 6000);
+                    
+                    function myStopFunction() {
+                        $(`.fila`).show()
+                    }
+                }     
+            }                
+        }))
+})
+
+// document.addEventListener("keyup", event => {
+//     console.log( event.target.matches(".coin"))
+//         // document.querySelectorAll(".fila").forEach(money => {
+//         //     money.textContent.toLowerCase().includes(event.target.value.toLowerCase())
+//         //         ?console.log("hola")
+//         //         :console.log("adios")
+//         // })
+// })
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    if (ty.value === "bitcoi" ){
+        $(`#bitcoin`).hide()
+        
+    }
+})
+
 
 
 
